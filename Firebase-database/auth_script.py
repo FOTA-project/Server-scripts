8935a799ed2b2ed499d5595d983d2d3a8d9ae2b4
Mina Helmi <59609326+MinaH94@users.noreply.github.com>
2020-05-30 18:59:33 +0200

    Added a test (upload and download) script

---
+++ Firebase-database/auth_script.py
@@ -0,0 +1,30 @@
+import pyrebase
+
+# upload
+firebaseConfig = {
+    "apiKey": "AIzaSyBgBFhNa6OnJCLbFTQW3vF_Cyz-rMyN4vU",
+    "authDomain": "fota-server-b4148.firebaseapp.com",
+    "databaseURL": "https://fota-server-b4148.firebaseio.com",
+    "projectId": "fota-server-b4148",
+    "storageBucket": "fota-server-b4148.appspot.com",
+    "messagingSenderId": "774423425890",
+    "appId": "1:774423425890:web:f506832444c3d30b2c323b",
+    "measurementId": "G-2DE9D9TN6N"
+  };
+
+# Initialize Firebase
+firebase = pyrebase.initialize_app(firebaseConfig);
+storage = firebase.storage()
+
+local_file = r'dma_mcal_test.elf'
+local_file_download = r'lolxd.elf'
+cloud_file = r'test_folder/test_upload.elf'
+
+# upload file
+storage.child(cloud_file).put(local_file)
+
+
+# download file
+storage.child(cloud_file).download(local_file_download)
+
+print("Script finished")
