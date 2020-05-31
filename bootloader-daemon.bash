39593b878e6331513d60740b9994416fe925e125
Mina Helmi <59609326+MinaH94@users.noreply.github.com>
2020-06-01 01:16:01 +0200

    Updated bootloader daemon

--- bootloader-daemon.bash
+++ bootloader-daemon.bash
@@ -1,21 +1,34 @@
 #! /bin/bash
 
+cd "$(dirname $0)"
 
-cd ~/Desktop/Firebase-database
-
+echo "step 1" > ~/Desktop/log.txt
+echo "### Downloading .elf file from server ###"
+cd Firebase-database/
 python3 download-script.py
+echo "######"
 
-cd ~/Desktop/ELF-parser/Python-script/
+echo "step 2" >> ~/Desktop/log.txt
+echo "### Moving .elf file to parser location ###"
+mv bootloader-dummy-app.elf ../ELF-parser/Python-script/
+echo "######"
 
-mv bootloader-dummy-app.elf ~/Desktop/ELF-parser/Python-script/
-
-cd ~/Desktop/ELF-parser/Python-script/
-
+echo "step 3" >> ~/Desktop/log.txt
+echo "### Parsing .elf file ###"
+cd ../ELF-parser/Python-script/
 python3 ELF_Parser.py
+echo "######"
 
-mv DATA_FILE.txt INFO_FILE.txt TEXT_FILE.txt ~/Desktop/RPI-communicator/
+echo "step 4" >> ~/Desktop/log.txt
+echo "### Moving parsed files to communicator location ###"
+mv DATA_FILE.txt INFO_FILE.txt TEXT_FILE.txt ../../RPI-communicator/bin/
+echo "######"
 
-cd ~/Desktop/RPI-communicator/
-
+echo "step 5" >> ~/Desktop/log.txt
+echo "### Executing communicator program ###"
+cd ../../RPI-communicator/bin/
 ./a
+echo "######"
+
+echo "step 6, final" >> ~/Desktop/log.txt
 
