43da853d89b4a34dec0cc59608d8c727a4ce4435
Mina Helmi <59609326+MinaH94@users.noreply.github.com>
2020-06-20 00:57:59 +0200

    Implemented a new feature to give the user a notification message when a new update is available, thanks to Sara
    -Decreased life polling time to 200ms
    
    -Added a new server flag for the new feature

--- bootloader-daemon.py
+++ bootloader-daemon.py
@@ -1,9 +1,14 @@
 import pyrebase
 import time
-import os
+import subprocess
 import threading
 import RPi.GPIO as GPIO
+import tkinter as tk
 from ELF_Parser import *
+from tkinter import *
+from tkinter import messagebox
+from tkinter.ttk import *
+
 
 GPIO.cleanup()
 
@@ -50,6 +55,18 @@ INSTRUCTION_TERMINATE_ON_SUCCESS  = -3
 INSTRUCTION_WRITE_MAX_REQUESTS    = -4
 #INSTRUCTION_GET_PROGRESS_FLAG_ARB = -5
 
+RPI_COMM_ERROR_SUCCESS                        =  0
+RPI_COMM_ERROR_COULDNT_OPEN_UART              = -1
+RPI_COMM_ERROR_COULDNT_OPEN_INFO_FILE         = -2
+RPI_COMM_ERROR_COULDNT_OPEN_DATA_FILE         = -3
+RPI_COMM_ERROR_COULDNT_OPEN_TEXT_FILE         = -4
+RPI_COMM_ERROR_COULDNT_OPEN_INSTRUCTIONS_FILE = -5
+RPI_COMM_ERROR_COULDNT_OPEN_PROGRESS_FILE     = -6
+RPI_COMM_ERROR_ACK_TIMEDOUT                   = -7
+RPI_COMM_ERROR_APP_SIZE_LARGE                 = -8
+
+rpiCommError = 0
+
 isTerminate = 0
 
 # syncchronization semaphore
@@ -80,8 +97,8 @@ def User_LifeRefresher_Thread():
     prevLifeFlag = -1
     
     while True:
-        # wait 400ms
-        time.sleep(0.4)
+        # wait 200ms
+        time.sleep(0.2)
         
         # we should attempt to get user data (uid, token) if it's being refreshed
         if isTokenRefreshThreadActive == 1:
@@ -106,7 +123,13 @@ def User_LifeRefresher_Thread():
 
 
 def RPi_Comm_Thread():
-    os.system('./a')
+    global rpiCommError
+    global RPI_COMM_ERROR_SUCCESS
+
+    rpiCommError = subprocess.call('./a')
+    if rpiCommError != RPI_COMM_ERROR_SUCCESS:
+        rpiCommError = rpiCommError- 256 # convert to signed
+    #print("rpiCommError = %d" %(rpiCommError))
 
 
 # user token refresher
@@ -139,13 +162,13 @@ while True:
     # get isNewElf flag from database
     isNewElf = db.child(user_db + "/isNewElf").get(user_tokenId).val()
 
-    if isNewElf == 0:
+    if isNewElf == 0: # if no new ELF file loop back again
        continue
 
     # download file
     storage.child(cloud_file).download(local_file_download, user_tokenId)
 
-    # update isNewElf flag in database
+    # reset isNewElf flag in database
     db.child(user_db).update({"isNewElf" : 0}, user_tokenId)
     
     # parse the downloaded file
@@ -162,6 +185,42 @@ while True:
     progressInstructionFile = open('progress.txt', 'rb')
     progressInstructionFile.seek(0, 0)
     
+    # ask the user if they want to apply the update
+    # create a hidden tkinter window to be used for user consent on  new update
+    # source: https://runestone.academy/runestone/books/published/thinkcspy/GUIandEventDrivenProgramming/02_standard_dialog_boxes.html
+    ask_for_update_window = tk.Tk()
+    ask_for_update_window.withdraw()
+
+    userAcceptedUpdate = messagebox.askyesno("New software update", "There's a new software update, it's highly recommended to apply it, do you want to continue ?", parent = ask_for_update_window)
+    
+    # destroy the window because it's kept even if the user pressed anything
+    ask_for_update_window.destroy()
+
+    # update isUserAcceptedUpdate flag in database so the GUI could continue
+    db.child(user_db).update({"isUserAcceptedUpdate" : int(userAcceptedUpdate)}, user_tokenId)
+    
+    if userAcceptedUpdate == False: # if user denied the update
+        db.child(user_db).update({"lastErrorCode" : RPI_COMM_ERROR_SUCCESS}, user_tokenId)
+        progressInstructionFile.close()
+        continue
+
+    # spawn updating progress window
+    updatingWindow = tk.Tk(className = ' Updating progress ')
+    updatingWindow.geometry("525x85")
+    updatingWindow.resizable(0, 0) # don't allow resizing in the x or y direction
+
+    updatingLabel = Label(updatingWindow, text = "Applying new update...", font = ('', 18), justify = CENTER)
+    extraLine = Label(updatingWindow, text = " ", font = ('arial', 8))
+
+    updatingWindowProgress = Progressbar(updatingWindow, orient = 'horizontal', length = 500, mode = 'determinate')
+    updatingWindowProgress['value'] = 0
+
+    updatingLabel.pack()
+    extraLine.pack()
+    updatingWindowProgress.pack()
+
+    updatingWindow.update_idletasks()
+
     # call RPi communicator
     commThreadHandle = threading.Thread(target = RPi_Comm_Thread)
     commThreadHandle.start()
@@ -181,6 +240,9 @@ while True:
    
     elfProgress = db.child(user_db + "/elfProgress").get(user_tokenId).val()
 
+    maxRequsets = 0
+    currentProgress = 0
+
     while isTerminate == 0:
         line = progressInstructionFile.readline().decode('utf-8').strip()
         #print("line = %s\n" %(line))
@@ -195,7 +257,8 @@ while True:
             isTerminate = 1
             #print("isTerminate = 1\n")
         elif int(line[:2], 10) == INSTRUCTION_WRITE_MAX_REQUESTS:
-            db.child(user_db).update({"elfProgressMaxRequest" : int(line.split()[1], 10)}, user_tokenId)
+            maxRequsets = int(line.split()[1], 10)
+            db.child(user_db).update({"elfProgressMaxRequest" : maxRequsets}, user_tokenId)
             #print("INSTRUCTION_WRITE_MAX_REQUESTS, %d\n" %(int(line.split()[1], 10)))
         elif int(line[:2], 10) == INSTRUCTION_GET_PROGRESS_FLAG:
             # goto position of requests count, and fill it from server
@@ -206,10 +269,31 @@ while True:
             #print("done\n")
             lastProgressFile.close()
         else: # normal +ve number
-            db.child(user_db).update({"elfProgress" : int(line, 10)}, user_tokenId)
+            currentProgress = int(line, 10)
+            db.child(user_db).update({"elfProgress" : currentProgress}, user_tokenId)
+            updatingWindowProgress['value'] = 100 * currentProgress / maxRequsets
+            updatingWindow.update_idletasks()
             #print("normal number, %d\n" %(int(line, 10)))
     
     progressInstructionFile.close()
+    updatingWindow.destroy()
     isTerminate = 0
+    
+    flashingFinalStatusWindow = tk.Tk()
+    flashingFinalStatusWindow.withdraw()
+
+    if rpiCommError == RPI_COMM_ERROR_SUCCESS:
+        db.child(user_db).update({"lastErrorCode" : RPI_COMM_ERROR_SUCCESS}, user_tokenId)
+        messagebox.showinfo("Update succeeded", "Successfully applied the new update!")
+    else:
+        userWantsToReport = messagebox.askyesno("Update failed", "There was an error during the update (error code = %d), send a report ?" %(rpiCommError), parent = flashingFinalStatusWindow)
+        if userWantsToReport:
+            db.child(user_db).update({"lastErrorCode" : rpiCommError}, user_tokenId)
+        else:
+            db.child(user_db).update({"lastErrorCode" : RPI_COMM_ERROR_SUCCESS}, user_tokenId)
+
+    # destroy the window because it's kept even if the user pressed anything
+    flashingFinalStatusWindow.destroy()
+
     #####commThreadHandle.stop() #############
 
--- start-bootloader-daemon.bash
+++ start-bootloader-daemon.bash
@@ -2,6 +2,9 @@
 
 cd "$(dirname $0)"
 
+# source: https://www.raspberrypi.org/forums/viewtopic.php?t=238070
+export DISPLAY=":0"
+
 while [ 1 ]
 do
    python3 bootloader-daemon.py 1>>"bootloader-daemon-log.txt" 2>>"bootloader-daemon-log.txt"
